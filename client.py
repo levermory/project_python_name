46b089e5ab4bce89e565283d07c9514ba1d7a18b
levermory <lev.schepelin@icloud.com>
2020-06-04 19:55:17 +0300

    added server functionality

---
+++ client.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from tkinter import *
+
+
+def update_score(score):
+    def show_message():
+        global nick
+        print(message_entry.get())
+        nick = message_entry.get()
+        root.destroy()
+
+
+    root = Tk()
+    root.title("Results")
+    root.geometry("300x250")
+
+    message_entry = Entry(textvariable="")
+    message_entry.place(relx=.5, rely=.1, anchor="c")
+
+    message_button = Button(text="Confirm", command=show_message)
+    message_button.place(relx=.5, rely=.5, anchor="c")
+
+    root.mainloop()
+
+    get = requests.get('http://127.0.0.1:8888')
+
+    data = get.json()
+    data[nick] = score
+    send = json.dumps(data)
+    requests.post('http://127.0.0.1:8888', json=send)
+
+update_score(500)
--- window.py
+++ window.py
@@ -9,6 +9,7 @@ from game_stats import GameStats
 from button import Button
 from scoreboard import Scoreboard
 
+
 def run_game():
     # Инициализирует игру и создает объект экрана.
     pygame.init()  # инициализирует настройки, необходимые Pygame для нормальной работы.
@@ -30,11 +31,11 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)  # Отслеживание событий клавиатуры и мыши.
         if ship.pause_flag == -1:
-        	if stats.game_active:
-        	    ship.update()
-        	    gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
-        	    gf.update_aliens(ai_settings, stats, screen, sb, ship, aliens, bullets)
-        	gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button, background_image)  # обновление экрана
+            if stats.game_active:
+                ship.update()
+                gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+                gf.update_aliens(ai_settings, stats, screen, sb, ship, aliens, bullets)
+            gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button, background_image)  # обновление экрана
         else:
-        	gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button, background_image)  # обновление экрана
+            gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button, background_image)  # обновление экрана
 run_game()
